9bace264b9a9afd5f5ae9f9616f382fde8274803
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-05-29 16:36:29 -0400

    Update TwitterReportCard.js

--- src/Components/TwitterReportCard.js
+++ src/Components/TwitterReportCard.js
@@ -4,19 +4,6 @@ import { toPng } from 'html-to-image';
 
 const TwitterReportCard = () => {
   const imageRef = useRef(null);
-  const [imageUrl, setImageUrl] = useState('');
-
-useEffect(() => {
-    toPng(imageRef.current).then((dataUrl) => {
-      console.log(1, dataUrl);
-      const img = new Image();
-      console.log(2, img);
-      img.src = dataUrl;
-        console.log(3, img.src);
-    //   document.body.appendChild(img);
-      setImageUrl(dataUrl);
-    });
-  }, []);
 
   return (
     <div>
@@ -29,7 +16,7 @@ useEffect(() => {
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Your Title" />
           <meta name="twitter:description" content="Your Description" />
-          <meta name="twitter:image" content={imageUrl} />
+          <meta name="twitter:image" content='https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80' />
         </Helmet>
       </>
       )}

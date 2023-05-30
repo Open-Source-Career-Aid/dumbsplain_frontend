fedab17f7d38362ab3e82f6ab5113d6a80219563
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-05-29 20:56:13 -0400

    Update TwitterReportCard.js

--- src/Components/TwitterReportCard.js
+++ src/Components/TwitterReportCard.js
@@ -6,9 +6,11 @@ const TwitterReportCard = () => {
     <div>
       <Helmet>
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Title" />
-        <meta name="twitter:description" content="Your Description" />
-        <meta name="twitter:image" content="https://example.com/your-image.jpg" />
+        <meta name="twitter:site" content="@nytimes" />
+        <meta name="twitter:creator" content="@SarahMaslinNir" />
+        <meta name="twitter:title" content="Parade of Fans for Houstons Funeral" />
+        <meta name="twitter:description" content="NEWARK" />
+        <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg" />
       </Helmet>
     </div>
   );

51698299a6d5a57390ee5a715df147c7a283ec73
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-06-04 13:01:36 -0400

    Update AnswerOptions.js

--- src/Components/AnswerOptions.js
+++ src/Components/AnswerOptions.js
@@ -11,91 +11,6 @@ function AnswerOptions ({ selectedoption, setSelectedoption, mcqrequested , setS
         }
     }
 
-    const option1 = classNames('answeroption', {
-        // eslint-disable-next-line
-        selected: selectedoption == 0 && correctoption == null
-    },
-    {
-        // eslint-disable-next-line
-        wrong: selectedoption == 0 && correctoption != null && correctoption != 0
-    },
-    {
-        // eslint-disable-next-line
-        correcthighlight: selectedoption != 0 && correctoption != null && correctoption == 0
-    },
-    {
-        // eslint-disable-next-line
-        correct: selectedoption == 0 && correctoption != null && correctoption == 0
-    });
-
-    const option2 = classNames('answeroption', {
-        // eslint-disable-next-line
-        selected: selectedoption == 1 && correctoption == null
-    },
-    {
-        // eslint-disable-next-line
-        wrong: selectedoption == 1 && correctoption != null && correctoption != 1
-    },
-    {
-        // eslint-disable-next-line
-        correcthighlight: selectedoption != 1 && correctoption != null && correctoption == 1
-    },
-    {
-        // eslint-disable-next-line
-        correct: selectedoption == 1 && correctoption != null && correctoption == 1
-    });
-
-    const option3 = classNames('answeroption', {
-        // eslint-disable-next-line
-        selected: selectedoption == 2 && correctoption == null
-    },
-    {
-        // eslint-disable-next-line
-        wrong: selectedoption == 2 && correctoption != null && correctoption != 2
-    },
-    {
-        // eslint-disable-next-line
-        correcthighlight: selectedoption != 2 && correctoption != null && correctoption == 2
-    },
-    {
-        // eslint-disable-next-line
-        correct: selectedoption == 2 && correctoption != null && correctoption == 2
-    });
-
-    const option4 = classNames('answeroption', {
-        // eslint-disable-next-line
-        selected: selectedoption == 3 && correctoption == null
-    },
-    {
-        // eslint-disable-next-line
-        wrong: selectedoption == 3 && correctoption != null && correctoption != 3
-    },
-    {
-        // eslint-disable-next-line
-        correcthighlight: selectedoption != 3 && correctoption != null && correctoption == 3
-    },
-    {
-        // eslint-disable-next-line
-        correct: selectedoption == 3 && correctoption != null && correctoption == 3
-    });
-
-    const option5 = classNames('answeroption', {
-        // eslint-disable-next-line
-        selected: selectedoption == 4 && correctoption == null
-    },
-    {
-        // eslint-disable-next-line
-        wrong: selectedoption == 4 && correctoption != null && correctoption != 4
-    },
-    {
-        // eslint-disable-next-line
-        correcthighlight: selectedoption != 4 && correctoption != null && correctoption == 4
-    },
-    {
-        // eslint-disable-next-line
-        correct: selectedoption == 4 && correctoption != null && correctoption == 4
-    });
-
     const answeroptions = classNames('answeroptions', {
         // eslint-disable-next-line
         block: correctoption != null
@@ -104,36 +19,38 @@ function AnswerOptions ({ selectedoption, setSelectedoption, mcqrequested , setS
     return (
         <>
             <div className={answeroptions}>
-                <div className={option1}
-                onClick={handleOptionclick}
-                value={0}>
-                    <div className="bgcontaineranswer"></div>
-                    <div className='answeroptiontext' data-theme={theme}>A</div>
-                </div>
-                <div className={option2}
-                onClick={handleOptionclick}
-                value={1}>
-                    <div className="bgcontaineranswer"></div>
-                    <div className='answeroptiontext' data-theme={theme}>B</div>
-                </div>
-                <div className={option3}
-                onClick={handleOptionclick}
-                value={2}>
-                    <div className="bgcontaineranswer"></div>
-                    <div className='answeroptiontext' data-theme={theme}>C</div>
-                </div>
-                <div className={option4}
-                onClick={handleOptionclick}
-                value={3}>
-                    <div className="bgcontaineranswer"></div>
-                    <div className='answeroptiontext' data-theme={theme}>D</div>
-                </div>
-                <div className={option5}
-                onClick={handleOptionclick}
-                value={4}>
-                    <div className="bgcontaineranswer"></div>
-                    <div className='answeroptiontext' data-theme={theme}>E</div>
-                </div>
+
+                {/* map from 1-5 for the answer options */}
+                {
+                    [1,2,3,4,5].map((item) => {
+                        return (
+                            <div
+                            className={classNames('answeroption', {
+
+                                // eslint-disable-next-line
+                                selected: selectedoption == item-1 && correctoption == null
+                            },
+                            {
+                                // eslint-disable-next-line
+                                wrong: selectedoption == item-1 && correctoption != null && correctoption != item-1
+                            },
+                            {
+                                // eslint-disable-next-line
+                                correcthighlight: selectedoption != item-1 && correctoption != null && correctoption == item-1
+                            },
+                            {
+                                // eslint-disable-next-line
+                                correct: selectedoption == item-1 && correctoption != null && correctoption == item-1
+                            })}
+                            onClick={handleOptionclick}
+                            value={item-1}
+                            key={item}>
+                                <div className="bgcontaineranswer"></div>
+                                <div className='answeroptiontext' data-theme={theme}>{String.fromCharCode(64+item)}</div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         </>
     );

6156d2e6e43fe7e8c592405094cad71e21d149b3
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-06-30 14:51:26 -0400

    --- Lami's updates

--- src/Components/ProgressChart.js
+++ src/Components/ProgressChart.js
@@ -8,7 +8,6 @@ import {
   Tooltip,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
 
 ChartJS.register(
   CategoryScale,
@@ -27,6 +26,23 @@ export const options = {
   },
   responsive: true,
   plugins: {
+        // tooltip: {
+        //   callbacks: {
+        //     label: (context) => {
+        //       const label = context.dataset.label || '';
+        //       const dataPoint = context.parsed.y;
+        //       return `${label}: ${dataPoint}`;
+        //     },
+        //   },
+        // },
+        verticalLine: {
+          display: true,
+          color: 'rgba(111, 111, 111, 1)',
+          dash: [3, 3],
+          width: 1,
+          xValue:'Day 7',
+        },
+
     legend: {
       position: 'top',
     },
@@ -39,7 +55,7 @@ export const options = {
     y: {
       display: false, // Hide y-axis
       suggestedMin: 0,
-      suggestedMax: 5,
+      suggestedMax: 7,
       grid: {
         drawBorder: false,
         drawOnChartArea: false,
@@ -47,12 +63,9 @@ export const options = {
     },
     x: {
       grid: {
-        color: ['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', '#000000', 'rgba(0, 0, 0, 0)'],
+        // color: ['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0)', '#000'],
         drawTicks: false,
-        // display: false, // Hide x-axis grid lines
-    },
-    border: {
-      dash: [5, 5],
+        display: false, // Hide x-axis grid lines
     },
     },
   },
@@ -65,7 +78,7 @@ export const data = {
   datasets: [
     {
       label: '',
-      data: labels.map(() => faker.datatype.number({ min: 1, max: 5 })),
+      data: [1, 2],
       borderColor: 'rgba(50, 188, 163, 1)',
       // backgroundColor: 'rgb(255, 99, 132)',
       fill: false,
@@ -73,7 +86,52 @@ export const data = {
   ],
 };
 
-function ProgressChart() {
+
+
+function ProgressChart({ linecolor , listofvalues }) {
+
+    const customPlugin = {
+        id: 'verticalLine',
+        afterDatasetsDraw: (chart) => {
+          const linePlugin = chart.options.plugins.verticalLine;
+          if (linePlugin.display) {
+            const ctx = chart.ctx;
+            const xScale = chart.scales['x'];
+            const yScale = chart.scales['y'];
+            const dataset = chart.data.datasets[0];
+            const lastIndex = dataset.data.length - 1;
+            const x = xScale.getPixelForValue(lastIndex);
+            const height = yScale.getPixelForValue(dataset.data[lastIndex]) - yScale.getPixelForValue(dataset.data[lastIndex]);
+            // score for Day 7 is the last datapoint
+            const y = dataset.data[lastIndex];
+            ctx.save();
+            ctx.beginPath();
+            ctx.setLineDash(linePlugin.dash);
+            ctx.lineWidth = linePlugin.width;
+            ctx.strokeStyle = linePlugin.color;
+            // draw score text at the contact point of the first datapoint use the y value of the first dataset
+            ctx.font = 'bold .5em Arial';
+            // add margin to the left of the text
+            // ctx.textBaseline = 'bottom';
+            ctx.fillStyle = 'rgba(50, 188, 163, 1)';
+            ctx.textAlign = 'center';
+            // text is placed on top of the contact point and offset it top of contact point by 20px
+            ctx.fillText(parseFloat(y), x, yScale.getPixelForValue(dataset.data[lastIndex]) + height - 10);
+            // move line to the contact point of the first datapoint
+            ctx.moveTo(x,yScale.getPixelForValue(dataset.data[lastIndex]) + height);
+            // ctx.moveTo(x, yScale.top - );
+            ctx.lineTo(x, yScale.bottom);
+            ctx.stroke();
+            ctx.restore();
+          }
+        },
+      };
+    
+      React.useEffect(() => {
+        ChartJS.register(customPlugin);
+      // eslint-disable-next-line
+      }, []);
+      
   return (
     <div style={{width: '100%', height: '100%'}}>
       <Line data={data} options={options} />

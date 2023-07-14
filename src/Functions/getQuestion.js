3ddbda870de9ba0852e78fe85d60c01473ffb2ab
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-07-14 18:24:46 -0400

    Update getQuestion.js

--- src/Functions/getQuestion.js
+++ src/Functions/getQuestion.js
@@ -15,6 +15,7 @@ export default async function getExplanation(dumbnessLevel) {
             level: dumbnessLevel,
         }),
     });
+    
     const data = await response.json();
 
     return data

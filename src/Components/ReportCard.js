d1ceb377322227ede5bdeac68b1d799a15bf40bf
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-07-26 14:25:12 -0400

    Update ReportCard.js

--- src/Components/ReportCard.js
+++ src/Components/ReportCard.js
@@ -243,7 +243,7 @@ const ReportCard = ({ scoreModal, setScoreModal , userdq , setUserdq , userstrea
 
         setTimeout(() => {
           setAllowcopy(true);
-        }, 5000);
+        }, 8000);
         
     // eslint-disable-next-line
     }, [scoreModal]);

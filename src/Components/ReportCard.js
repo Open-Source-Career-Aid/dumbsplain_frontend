156b9451f9728ca990d8fbbe61ca1297b4ba7644
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-07-26 21:22:12 -0400

    Update ReportCard.js

--- src/Components/ReportCard.js
+++ src/Components/ReportCard.js
@@ -51,8 +51,34 @@ const ReportCard = ({ scoreModal, setScoreModal , userdq , setUserdq , userstrea
               height: element.offsetHeight + "px",
             },
           })
-          .then((blob) => {
-            resolve(blob);
+          .then(() => {
+            domtoimage
+            .toBlob(element, {
+              height: element.offsetHeight * scale,
+              width: element.offsetWidth * scale,
+              style: {
+                transform: "scale(" + scale + ")",
+                transformOrigin: "top left",
+                width: element.offsetWidth + "px",
+                height: element.offsetHeight + "px",
+              },
+            })
+            .then(() => {
+              domtoimage
+              .toBlob(element, {
+                height: element.offsetHeight * scale,
+                width: element.offsetWidth * scale,
+                style: {
+                  transform: "scale(" + scale + ")",
+                  transformOrigin: "top left",
+                  width: element.offsetWidth + "px",
+                  height: element.offsetHeight + "px",
+                },
+              })
+              .then((blob) => {
+                resolve(blob);
+              });
+            });
           });
       } catch (e) {
         reject(e);

ecc320d207622cb5ce8391ebdb680f4eaa8b6775
Liza McLain <lizamclain@gmail.com>
2023-11-14 12:03:45 -0600

    basic setup of page and component

---
+++ src/Components/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function SignupPage() {
+    return (
+        <div class="flex items-center justify-center min-h-screen">
+            <h1 class="font-bold text-center">
+                Do you have an .edu address?
+            </h1>
+        </div>
+    )
+}

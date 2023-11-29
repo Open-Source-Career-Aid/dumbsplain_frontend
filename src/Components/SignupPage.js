ab1adfd01348e05afe48b673d7fe11183f1128eb
Liza McLain <lizamclain@gmail.com>
2023-11-29 17:21:57 -0600

    more fields and forms

--- src/Components/SignupPage.js
+++ src/Components/SignupPage.js
@@ -23,13 +23,13 @@ export default function SignupPage() {
 
     if(showLanding) return (
         <div>
-            <h1 className="tw-font-bold tw-text-center">
-                Do you have an .edu address?
+            <h1 className="tw-font-bold tw-text-center tw-mb-6 tw-mt-8">
+                Do you have an .edu email address?
             </h1>
-            <button onClick={handleYesClick} className="tw-rounded-xl tw-bg-blue tw-text-white tw-border tw-border-white tw-hover:bg-orange tw-w-full tw-lg:w-auto tw-my-2">
+            <button onClick={handleYesClick} className="tw-rounded-xl tw-bg-blue_400 hover:tw-bg-orange_200 tw-text-white tw-border tw-border-white tw-w-full tw-lg:w-auto tw-my-2">
                 Yes
             </button>
-            <button onClick={handleNoClick} className="tw-rounded-xl tw-bg-white tw-border tw-border-blue tw-w-full tw-lg:w-auto">
+            <button onClick={handleNoClick} className="tw-rounded-xl tw-bg-white hover:tw-bg-orange_200 hover:tw-text-white hover:tw-border-orange_200 tw-border tw-border-blue_400 tw-w-full tw-lg:w-auto">
                 No
             </button>
         </div>
@@ -37,22 +37,38 @@ export default function SignupPage() {
 
     if(showEdu) return (
         <div>
-            <h1 className="tw-font-bold tw-text-center">
+            <h1 className="tw-font-bold tw-text-center tw-mb-6 tw-mt-8">
                 Please sign up with your .edu email address.
             </h1>
             <form>
-                <div>
-                    <label for="Helena Bonham Carter">Name</label>
-                    <input></input>
+                <div className="tw-my-2 tw-flex tw-flex-col tw-mb-1">
+                <label className="tw-font-bold tw-text-xs">Name</label>
+                <input
+                    className="tw-rounded-lg tw-border tw-border-neutral_300 tw-py-1 tw-px-1 tw-text-xs tw-w-1/2 tw-ml-2"
+                    id="name"
+                    type="text"
+                    placeholder="Helena Bonham Carter"
+                ></input>
+                </div>
+                <div className="tw-flex tw-flex-col tw-mb-4">
+                    <label className="tw-font-bold tw-text-xs">Email</label>
+                    <input
+                        className="tw-rounded-lg tw-border tw-border-neutral_300 tw-py-1 tw-px-1 tw-text-xs tw-w-1/2 tw-ml-2"
+                        id="email"
+                        type="text"
+                        placeholder="hcarter@university.edu">
+                    </input>
                 </div>
                 <div>
-                    <label>Email</label>
-                    <input></input>
+                    <button className="tw-rounded-xl tw-bg-blue_400 hover:tw-bg-orange_200 tw-text-white tw-border tw-border-white tw-w-1/2 tw-my-2" type="button">
+                        Continue
+                    </button>
                 </div>
             </form>
         </div>
     )
 }
 
-// fix buttons
-// border, hover colors, click colors, spacing, padding
+// fix input fields being in the center
+// button click colors
+// might want to fix the tw-ml-2 for the input field
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: '#4C7BFE',
-        orange: 'F86E42'
+        blue_400: '#4C7BFE',
+        orange_300: '#F86E42',
+        orange_200: '#F49577',
+        neutral_300: '#ABB0C2',
       },
     },
   },

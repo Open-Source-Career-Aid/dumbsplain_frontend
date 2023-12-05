2047126932a119f1e91d79acd1c02a6d4fd55020
Lami <olamide2olowoniyi@gmail.com>
2023-12-04 20:44:01 -0500

    revamped getleaderboard

--- src/Functions/getLeaderBoardRanks.js
+++ src/Functions/getLeaderBoardRanks.js
@@ -1,13 +1,33 @@
-import { APIURL } from '../config.js'
-export default async function getLeaderBoard(mode=1) {
-    const url = `${APIURL}/api/getleaderboard/`;
-    const response = await fetch(url, { 'credentials': 'include', 'crossDomain': true, 'method': 'GET', 'headers': { 'Content-Type': 'application/json' }, 'body': JSON.stringify({ 'type': mode })});
-    const data = await response.json();
-    console.log(data);
-    if (typeof (Storage) !== 'undefined') {
-    localStorage.setItem('lastFetched', Date.now());
-    localStorage.setItem(`boardType${mode}`, mode);
-    }
-    return data;
+import { APIURL } from '../config.js';
 
+export default async function getLeaderBoard(mode = 0) {
+    const url = `${APIURL}/api/getleaderboard/?type=${mode}`;
+    
+    try {
+        const response = await fetch(url, {
+            credentials: 'include',
+            crossDomain: true,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        if (typeof Storage !== 'undefined') {
+            localStorage.setItem('lastFetched', Date.now());
+            localStorage.setItem(`boardType${mode}`, mode);
+        }
+
+        return data;
+    } catch (error) {
+        console.error('Error:', error.message);
+        return [];
+    }
 }

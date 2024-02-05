3a178dad9c33c9aecfa50cc65258a5168ddcb690
Lami <olamide2olowoniyi@gmail.com>
2024-02-04 22:48:26 -0500

    fix!: updated to serve production endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// export const APIURL='https://api.dumbsplain.com';
-export const APIURL='http://127.0.0.1:8000';
+export const APIURL='https://api.dumbsplain.com';
+// export const APIURL='http://127.0.0.1:8000';
 export const avatarlabels = ['Just Plain Dumb', 'Not Too Bright', 'Smartass', 'Pretentious Professor', 'Sentient Savant']
 export const GTAG = 'G-5F2TKF5294'
